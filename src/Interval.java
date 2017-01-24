33d31c3d255c05e179bd35c4ab81565a78c73944
Dan-gem <Dan_dan_gen@mail.ru>
2017-01-24 18:14:49 +0600

    top

--- src/Interval.java
+++ src/Interval.java
@@ -4,30 +4,67 @@ public class Interval {
     public static void main(String[] args) {
         System.out.println("Введите число от 0 до 100 и программа отределит в какой числовой промежуток вы попали: ");
         Scanner in = new Scanner(System.in); // Объект класса Scanner для обработки ввода с клавиатуры.
-        String a = in.next();
+        int f = in.nextInt();
+
+//        String o;
+
+        boolean a = 0 <= f && f <= 14;
+        boolean b = 15 <= f && f <= 35;
+        boolean c = 36 <= f && f <= 50;
+        boolean e = 50 <= f && f <= 100;
+
+//        o = (f.equals(a))?(System.out.println("Вы попали в диапазон от 0 до 14")):(" ");
+
+//        boolean a = (0 <= f && f >= 14);
+//            if (f.equals(a)) {
+//            System.out.println("Вы попали в диапазон от 0 до 14");
+//
+//            boolean b = 15 <= f && f >= 35;
+//            if (f.equals(b)) {
+//                System.out.println("Вы попали в диапазон от 15 до 35");
+//            } else {
+//
+//            }
+//            boolean c = 36 <= f && f >= 50;
+//            if (f.equals(c)) {
+//                System.out.println("Вы попали в диапазон от 36 до 50");
+//            } else {
+//
+//            }
+//            boolean e = 50 <= f && f >= 100;
+//            if (f.equals(e)) {
+//                System.out.println("Вы попали в диапазон от 50 до 100");
+//            } else {
+//
+//            }
+
 
         //String b = in.next();
 
-        switch (a) {
-            case "<=14": { //"(0>=a&&a<=14)"
+        switch (f) {
+            case "a": {
                 System.out.println("Вы попали в диапазон от 0 до 14");
                 break;
             }
-            case "(a<=35)": {
+
+            case "b": {
 
                 System.out.println("Вы попали в диапазон от 15 до 35");
                 break;
             }
-            case "(36>=a<=50)": {
+
+            case "c": {
 
                 System.out.println("Вы попали в диапазон от 36 до 50");
                 break;
             }
-            case "(50>=a<=100)": {
+
+            case "e": {
 
                 System.out.println("Вы попали в диапазон от 50 до 100");
                 break;
             }
+
             default: {
                 System.out.println("введите число от 0 до 100");
                 break;
@@ -37,4 +74,7 @@ public class Interval {
         }
     }
 }
+
+
+
 
