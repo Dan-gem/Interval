3f5e4cc215476c1b91123ab58c59f9aab944f66c
Dan-gem <Dan_dan_gen@mail.ru>
2017-01-25 18:18:02 +0600

    top1

--- src/Interval.java
+++ src/Interval.java
@@ -2,78 +2,122 @@ import java.util.Scanner;
 
 public class Interval {
     public static void main(String[] args) {
-        System.out.println("Введите число от 0 до 100 и программа отределит в какой числовой промежуток вы попали: ");
+        System.out.println("Введите число от 0 до 100 и программа отределит в какой числовой диапазон вы попали: ");
         Scanner in = new Scanner(System.in); // Объект класса Scanner для обработки ввода с клавиатуры.
-        int f = in.nextInt();
+//        int f = in.nextInt();
+        int p = in.nextInt();
+        String s;
+        String a;
 
-//        String o;
+        boolean e = (p>100);
+        boolean s = (p<0);
 
-        boolean a = 0 <= f && f <= 14;
-        boolean b = 15 <= f && f <= 35;
-        boolean c = 36 <= f && f <= 50;
-        boolean e = 50 <= f && f <= 100;
+//        String d;
 
-//        o = (f.equals(a))?(System.out.println("Вы попали в диапазон от 0 до 14")):(" ");
+        if (e) {
+            System.out.println("Число не попадает в диапазон от 0 до 100, введите число еще раз: ");
+        } else {
+            a = (p<=35)?((p<=14)?"0-14":"15-35"):((p<=50)?"36-50":"51-100");
+            System.out.println(a);
+        }
 
-//        boolean a = (0 <= f && f >= 14);
-//            if (f.equals(a)) {
-//            System.out.println("Вы попали в диапазон от 0 до 14");
+//            a = (p<=35)?((p<=14)?"0-14":"15-35"):((p<=50)?"36-50":"51-100");
+//            System.out.println(a);
+
+
+
+
+
+//        int q;
+//        int a;
+//        int b;
+//        int c;
+//        int e;
+
+//        String u;
+//        String i;
+//        String x;
 //
-//            boolean b = 15 <= f && f >= 35;
-//            if (f.equals(b)) {
-//                System.out.println("Вы попали в диапазон от 15 до 35");
-//            } else {
+//        s = (0<=p&&p<=15)?"a":"u";
+//        u = (16<=p&&p<=36)?"b":"i";
+//        i = (37<=p&&p<=50)?"c":"x";
+//        x = (51<=p&&p<=100)?"e":"";
 //
+//        switch ("s") {
+//
+//            case "a": {
+//                System.out.println("0-15");
+//                break;
 //            }
-//            boolean c = 36 <= f && f >= 50;
-//            if (f.equals(c)) {
-//                System.out.println("Вы попали в диапазон от 36 до 50");
-//            } else {
-//
+//            case "b": {
+//                System.out.println("15-36");
+//                break;
 //            }
-//            boolean e = 50 <= f && f >= 100;
-//            if (f.equals(e)) {
-//                System.out.println("Вы попали в диапазон от 50 до 100");
-//            } else {
-//
+//            case "c": {
+//                System.out.println("36-50");
+//                break;
+//            }
+//            case "e": {
+//                System.out.println("50-100");
+//                break;
 //            }
 
+//        switch ("s") {
+//
+//            case "36>p": {
+//                s = (0<=p&&p<=15)?"0-15":"15-36";
+//                System.out.println("s");
+//                break;
+//            }
+//
+//            case "36<p": {
+//                s = (0<=p&&p<=15)?"36-50":"50-100";
+//                System.out.println("s");
+//                break;
+//            }
 
-        //String b = in.next();
-
-        switch (f) {
-            case "a": {
-                System.out.println("Вы попали в диапазон от 0 до 14");
-                break;
-            }
-
-            case "b": {
-
-                System.out.println("Вы попали в диапазон от 15 до 35");
-                break;
-            }
-
-            case "c": {
-
-                System.out.println("Вы попали в диапазон от 36 до 50");
-                break;
-            }
-
-            case "e": {
-
-                System.out.println("Вы попали в диапазон от 50 до 100");
-                break;
-            }
-
-            default: {
-                System.out.println("введите число от 0 до 100");
-                break;
-            }
+//        s = (0<=p&&p<=15)?"0-15":q;
+//        q = (16<=p&&p<=35)?"16-35":q1;
 
 
+                         //  "0-15":((16<=p&&p<=35)?"15-35":((35<=p&&p<=50)?"35-50":(50<=p&&p<=100)?"50-100":" "));
+//        s = (0<=p&&p<=15)?"0-15":((16<=p&&p<=35)?"15-35":((35<=p&&p<=50)?"35-50":(50<=p&&p<=100)?"50-100":" "));
+//        s = (0 <= p && p <= 15) ? "0-15" : ((16 <= p && p <= 35) ? "15-35" : ((35 <= p && p <= 50) ? "35-50" : (50 <= p && p <= 100) ? "50-100" : " "));
+//        System.out.println(s);
+
+//        int x = 10, y = -5;
+//        String q0, q1, q2;
+//        q0 = (f>0) ? ((p>0) ? "1 квадрант" : "4 квадрант") : ((p>0) ? "2 квадрант" : "3 квадрант");
+
+//        System.out.println (q0);
+
+//        switch ("s") {
+//            case "0 <= p && p <= 15":
+//                System.out.println("Ответ:"+"0-15");
+//                break;
+//
+//            case "16 <= p && p <= 35": {
+//                System.out.println("Ответ:"+"16-35");
+//                break;
+//            }
+//            case "36 <= p && p <= 50": {
+//                    System.out.println("Ответ:"+"36-50");
+//                    break;
+//            }
+//            case "51 <= p && p <= 100": {
+//                System.out.println("Ответ:"+"50-100");
+//                break;
+//            }
+//            default: {
+//                System.out.println("введите знак арифметической операции");
+//                break;
+//            }
+
+//
         }
     }
-}
+
+
 
 
 
